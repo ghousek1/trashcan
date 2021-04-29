728da5bd27e1f15baa4ea78183e1d4a25eb071a8
ghouse <thegouse369@gmail.com>
2021-04-29 19:53:24 +0530

    Commit 29-Apr-21 _ 19:53:24.53

--- groundhog.bat
+++ groundhog.bat
@@ -5,15 +5,27 @@ CLS
 
 ::ECHO Testing Git Committer
 
+:: Add CLS(clear terminal) for every Line to hide execution
+
 git checkout main
+
+CLS
 
 git pull
 
+CLS
+
 git add .
+
+CLS
 
 git status -s
 
+CLS
+
 git commit -m "Commit %DATE% _ %TIME% "
+
+CLS
 
 git push -u origin main
 
