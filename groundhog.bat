84898c7f8342695c9cdaadbf8a701a22ac515e45
ghouse <thegouse369@gmail.com>
2021-04-29 19:41:12 +0530

    testing command prompt git commit

--- groundhog.bat
+++ groundhog.bat
@@ -1,10 +1,21 @@
+::grooundhog/otter/ferret
 ECHO OFF
 CLS
 ECHO Testing Git Committer
+
+echo %DATE%
+echo %TIME%
+
 git checkout main
+
+git pull
+
 git add .
+
 git status -s
+
 git commit -m "testing command prompt git commit"
-git push -u origin master
+
+git push -u origin main
 
 ECHO git pushed to the origin completed
